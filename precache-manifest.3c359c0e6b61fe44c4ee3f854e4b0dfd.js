b5b72997dca3d8c0bef0ef4489c7b86d6301f059
Elio Mattia <elio.mattia@gmail.com>
2020-02-08 23:40:15 +0100

    Updates

---
+++ precache-manifest.3c359c0e6b61fe44c4ee3f854e4b0dfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a2ae40285df3e613051afdd8315df0e",
+    "url": "/thrubi-client/index.html"
+  },
+  {
+    "revision": "54125edd0f6a6b0d2f7d",
+    "url": "/thrubi-client/static/css/main.cf24463d.chunk.css"
+  },
+  {
+    "revision": "5efc0553a2ab1c147a71",
+    "url": "/thrubi-client/static/js/2.674afcba.chunk.js"
+  },
+  {
+    "revision": "54125edd0f6a6b0d2f7d",
+    "url": "/thrubi-client/static/js/main.04c50301.chunk.js"
+  },
+  {
+    "revision": "47b1d2e5caf3aad1bd73",
+    "url": "/thrubi-client/static/js/runtime~main.0651016d.js"
+  }
+]);

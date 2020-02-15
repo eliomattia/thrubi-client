4fa68c86e448b241ca7df2e9d04b90d6b1b5d540
Elio Mattia <elio.mattia@gmail.com>
2020-02-15 13:59:47 +0100

    Updates

---
+++ precache-manifest.3d545e91ab36980398e632e59df49907.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df74be974b5bddfcd4e530b10da1c9ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7d09ff2edd77f438582",
+    "url": "/static/css/main.88227cbc.chunk.css"
+  },
+  {
+    "revision": "e6ae9c9b8d3db2270bad",
+    "url": "/static/js/2.4adf6b09.chunk.js"
+  },
+  {
+    "revision": "8565c4aafb1b360e7ccc374a77dc79c6",
+    "url": "/static/js/2.4adf6b09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7d09ff2edd77f438582",
+    "url": "/static/js/main.e5765bf5.chunk.js"
+  },
+  {
+    "revision": "e38b6a1136fb07c9189a",
+    "url": "/static/js/runtime-main.bf4e0b89.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/thrubi-client/precache-manifest.ffb3272d988e22df0cfce8601bff18e1.js"
+  "/precache-manifest.3d545e91ab36980398e632e59df49907.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/thrubi-client/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

582c69cab4b5ad8ac4352283b1b33ff35f90ff0d
Elio Mattia <elio.mattia@gmail.com>
2020-03-05 01:19:27 +0100

    Restyled and refactored code for header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,29 @@
-import React,{Fragment} from "react";
+import React from "react";
 import {connect} from "react-redux";
 import GlobalStatsUsers from "./GlobalStatsUsers";
 import GlobalStatsCountryCcy from "./GlobalStatsCountryCcy";
+import EthBadges from "./EthBadges";
 
 const _Footer = ({ethNetwork,ethAddress}) => (
-    <Fragment>
-        <div className="d-block d-lg-none container-fluid flex-md-nowrap p-0 footer">
-            <div className="col-lg-12 float-right m-0 py-2 pt-2 text-center bg-light"
-                 style={{backgroundImage: "url("+process.env.PUBLIC_URL+"/logo.png",
-                     backgroundSize: "contain",
-                     backgroundPosition: "center",
-                     backgroundRepeat:"no-repeat",
-                     backgroundBlendMode:"color-dodge"}}>
-                <GlobalStatsUsers />
-                <span className="small d-block"><GlobalStatsCountryCcy/></span>
-                <span className="mb-2 small">Public data provided by:</span>
-                <br/>
+    <div className="bg-light flex-grow-1"
+         style={{backgroundImage: "url("+process.env.PUBLIC_URL+"/logo.png",
+             backgroundSize: "contain",
+             backgroundPosition: "center",
+             backgroundRepeat:"no-repeat",
+             backgroundBlendMode:"color-dodge"}}>
+        <div className="container row m-0 p-0 p-lg-4 footer">
+            <div className="col-lg-4 m-0 px-0 py-2 py-lg-3 text-center text-lg-left">
+                <div className="d-block d-lg-none mt-3">
+                    <GlobalStatsUsers />
+                    <span className="small d-block"><GlobalStatsCountryCcy/></span>
+                </div>
+                <p className="m-0 p-0 mt-3 mt-lg-0">Public data provided by:</p>
                 <a href="https://www.irs.gov" target="_blank" rel="noopener noreferrer"><img className="text-center d-inline mr-1 mb-1" alt="IRS.gov" height="80px" src={process.env.PUBLIC_URL+"/jpg/IRS.gov.jpg"} /></a>
                 <a href="https://wid.world" target="_blank" rel="noopener noreferrer"><img className="text-center d-inline mr-1 mb-1" alt="wid.world" height="80px" src={process.env.PUBLIC_URL+"/png/wid.world.png"} /></a>
-                <br/>
-                <div className="px-1">
+                <div className="d-none d-lg-block mt-3">
+                    <GlobalStatsCountryCcy/>
+                </div>
+                <div className="mt-3 px-1 py-lg-0 py-lg-1">
                     <img className="text-center d-inline mr-1 mb-1" alt="email" width="20px" src={process.env.PUBLIC_URL+"/png/email.png"} style={{filter:"opacity(.5) sepia(1) hue-rotate(180deg)"}} />
                     &nbsp;
                     <a className="text-center text-secondary" href="mailto:info@thrubi.com">
@@ -28,25 +32,21 @@ const _Footer = ({ethNetwork,ethAddress}) => (
                 </div>
                 <a href="https://twitter.com/thrubi_org?ref_src=twsrc%5Etfw" data-show-count="false" className="twitter-follow-button">Follow @thrubi_org</a>
                 <br/>
-                <a href="https://www.patreon.com/bePatron?u=30859926" data-patreon-widget-type="become-patron-button">Become a Patron!</a>
+                <a href="https://www.patreon.com/bePatron?u=30859926" data-patreon-widget-type="become-patron-button" className="mt-0 mt-lg-4">Become a Patron!</a>
+                <div className="d-block d-lg-none mt-3">
+                    <EthBadges position="F" />
+                </div>
             </div>
-            {
-                !ethNetwork || !ethAddress ? "" :
-                    <div className="col-lg-12 float-right mx-0 my-0 pb-1 text-center bg-light">
-                        <div className="navbar-nav small consoleFont">
-                            <div>
-                                <span className="badge badge-warning d-inline"><span className="">network/</span>{ethNetwork}</span>
-                                <span className="badge badge-info d-inline">{ethAddress}</span>
-                            </div>
-                        </div>
-                    </div>
-            }
-            <div className="col-lg-12 float-right mx-0 my-0 pb-1 text-center bg-light">
+            <div className="col-lg-4 m-0 px-0 pb-1 py-lg-3 text-center text-secondary">
                 <a href="https://discord.gg/ZUHPgyt" target="_blank" rel="noopener noreferrer">
                     <img className="text-center d-inline mx-0 my-3" alt="discord" height="50px" src={process.env.PUBLIC_URL+"/png/discord_grey.png"} />
                 </a>
+                <div className="d-none d-lg-block">
+                    <iframe title="discordFrame" src="https://discordapp.com/widget?id=681677187044343841&theme=dark" width="100%" height="250"
+                            allowTransparency="true" frameBorder="0" />
+                </div>
             </div>
-            <div className="col-lg-12 float-right m-0 p-0 text-center text-success bg-light">
+            <div className="col-lg-4 m-0 p-0 py-lg-3 text-center text-lg-right text-success">
                 <span className="d-block navbar-nav small">
                     <p className="d-inline-block">Rising inequality is toxic to growth</p>
                     <i className="d-inline-block small">&nbsp;&mdash; Nick Hanauer</i>
@@ -54,51 +54,10 @@ const _Footer = ({ethNetwork,ethAddress}) => (
                 <div className="pt-0 pb-3">© 2020 Thrubi.com</div>
             </div>
         </div>
-        <div className="d-none d-lg-flex flex-md-nowrap m-0 p-4 container-fluid flex-grow-1 footer bg-light"
-             style={{backgroundImage: "url("+process.env.PUBLIC_URL+"/logo.png",
-                 backgroundSize: "contain",
-                 backgroundPosition: "center",
-                 backgroundRepeat:"no-repeat",
-                 backgroundBlendMode:"color-dodge"}}>
-            <div className="col-lg-4 float-right m-0 px-0 py-3 text-left">
-                <p className="m-0 p-0 mb-3">Public data provided by:</p>
-                <a href="https://www.irs.gov" target="_blank" rel="noopener noreferrer"><img className="text-center d-inline mr-1 mb-1" alt="IRS.gov" height="80px" src={process.env.PUBLIC_URL+"/jpg/IRS.gov.jpg"} /></a>
-                <a href="https://wid.world" target="_blank" rel="noopener noreferrer"><img className="text-center d-inline mr-1 mb-1" alt="wid.world" height="80px" src={process.env.PUBLIC_URL+"/png/wid.world.png"} /></a>
-                <p className="m-0 p-0 mt-3"/>
-                <GlobalStatsCountryCcy/>
-                <div className="py-1">
-                    <img className="text-center d-inline mr-1 mb-1" alt="email" width="20px" src={process.env.PUBLIC_URL+"/png/email.png"} style={{filter:"opacity(.5) sepia(1) hue-rotate(180deg)"}} />
-                    &nbsp;
-                    <a className="text-center text-secondary" href="mailto:info@thrubi.com">
-                        info@thrubi.com
-                    </a>
-                </div>
-                <a href="https://twitter.com/thrubi_org?ref_src=twsrc%5Etfw" data-show-count="false" className="twitter-follow-button">Follow @thrubi_org</a>
-                <br/>
-                <a href="https://www.patreon.com/bePatron?u=30859926" data-patreon-widget-type="become-patron-button" className="mt-4">Become a Patron!</a>
-            </div>
-            <div className="col-lg-4 float-right m-0 px-0 py-3 text-center text-secondary">
-                <a href="https://discord.gg/ZUHPgyt" target="_blank" rel="noopener noreferrer">
-                    <img className="text-center d-inline mx-0 mb-3" alt="discord" height="50px" src={process.env.PUBLIC_URL+"/png/discord_grey.png"} />
-                </a>
-                <br />
-                <iframe src="https://discordapp.com/widget?id=681677187044343841&theme=dark" width="350" height="300"
-                        allowTransparency="true" frameBorder="0" />
-            </div>
-            <div className="col-lg-4 float-right m-0 px-0 py-3 text-right text-success">
-                <span className="d-block navbar-nav">
-                    <p className="d-inline-block">Rising inequality is toxic to growth</p>
-                    <i className="d-inline-block small">&nbsp;&mdash; Nick Hanauer</i>
-                </span>
-                <div className="pb-3">© 2020 Thrubi.com</div>
-            </div>
-        </div>
-    </Fragment>
+    </div>
 );
 
 const mapStateToProps = state => ({
-    ethNetwork: state.client.userAccess.ethNetwork,
-    ethAddress: state.client.userAccess.ethAddress,
 });
 
 const Footer = connect(mapStateToProps)(_Footer);
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {connect} from "react-redux";
 import GlobalStatsUsers from "./GlobalStatsUsers";
+import EthBadges from "./EthBadges";
 
 const _Header = ({ethNetwork,ethAddress}) => (
     <div className="container-fluid navbar fixed-top flex-md-nowrap p-0 titleBar bg-light">
@@ -15,23 +16,11 @@ const _Header = ({ethNetwork,ethAddress}) => (
             <span className="mr-2 alignBottom navbar-brand thrubiGradient"><h4><b>Thrubi</b></h4></span>
             <div className="d-none d-lg-block alignBottom"><GlobalStatsUsers/></div>
         </div>
-        <div className="col-lg-4 d-none d-lg-block float-right mr-0 my-2 text-right consoleFont">
-            <div className="navbar-nav small alignBottom">
-                {
-                    !ethNetwork || !ethAddress ? "" :
-                    <div>
-                        <span className="badge badge-warning d-none d-xl-inline"><span className="">network/</span>{ethNetwork}</span>
-                        <span className="badge badge-info d-inline">{ethAddress}</span>
-                    </div>
-                }
-            </div>
-        </div>
+        <EthBadges position="H" />
     </div>
 );
 
 const mapStateToProps = state => ({
-    ethNetwork: state.client.userAccess.ethNetwork,
-    ethAddress: state.client.userAccess.ethAddress,
 });
 
 const Header = connect(mapStateToProps)(_Header);

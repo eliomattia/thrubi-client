98210ff841a0366a79c489718b5e04506ab40361
Elio Mattia <elio.mattia@gmail.com>
2020-03-08 17:16:13 +0100

    input type="password"

--- src/components/_KeyboardChannel.js
+++ src/components/_KeyboardChannel.js
@@ -7,7 +7,7 @@ const _KeyboardChannel = ({text,action,abandonAction,buttonType,abandonButtonTyp
         <div className="my-2">
             <span><b>Your credentials:</b></span>
             <input id="username" ref={input => refs.username = input} type="text" className="form-control form-control-sm rounded-0 my-2" placeholder="username" required />
-            <input id="password" ref={input => refs.password = input} type="text" className="form-control form-control-sm rounded-0 my-2" placeholder="password" required />
+            <input id="password" ref={input => refs.password = input} type="password" className="form-control form-control-sm rounded-0 my-2" placeholder="password" required />
             <_ActionButton text={text} action={() => {action(refs.username.value,refs.password.value);}} buttonType={buttonType}/>
             <_ActionButton text="Back" action={abandonAction} buttonType={abandonButtonType}/>
             <span className="small"><i>Your password will be encrypted. We do not store plain text passwords on our servers. Verify your email address to make your account more secure.</i></span>

0cd1c0a8a171f7a4400b0f89386f3e6e97b5f2d9
Elio Mattia <elio.mattia@gmail.com>
2020-04-04 18:59:09 +0200

    Redirect page

--- src/index.js
+++ src/index.js
@@ -1,18 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import {Route} from "react-router";
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from "react-redux";
-import thrubiApp from "./reducers/thrubiApp"
-import App from "./components/App";
-import "./styles/customBootstrap.scss";
-
-const store = thrubiApp();
-ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
-            <Route exact path="/"                   component={App} />
-        </BrowserRouter>
-    </Provider>,
-    document.getElementById("root")
-);
+console.log("Redirecting to https://www.thrubi.org");
